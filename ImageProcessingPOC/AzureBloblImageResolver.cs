0667f022e1bc3187fc096a9980471d426f313cef
Bart Donninger <bart.donninger@pggmenco.nl>
2018-07-30 14:04:05 +0200

    Poc for image processing middleware for azure blob storage

---
+++ ImageProcessingPOC/AzureBloblImageResolver.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Web.Helpers;
+using SixLabors.ImageSharp.Web.Memory;
+using SixLabors.ImageSharp.Web.Middleware;
+using SixLabors.ImageSharp.Web.Resolvers;
+using IBufferManager = SixLabors.ImageSharp.Web.Memory.IBufferManager;
+
+namespace ImageProcessingPOC
+{
+    /// <summary>
+    ///     Returns images stored in an azure blob container
+    /// </summary>
+    public class AzureBlobImageResolver : IImageResolver
+    {
+        private readonly IBufferManager _bufferManager;
+        private readonly AzureBlobContainerSettings _containerSettings;
+        private readonly ImageSharpMiddlewareOptions _options;
+
+        public AzureBlobImageResolver(AzureBlobContainerSettings containerSettings,
+            IBufferManager bufferManager,
+            IOptions<ImageSharpMiddlewareOptions> options)
+        {
+            _containerSettings = containerSettings;
+            _bufferManager = bufferManager;
+            _options = options.Value;
+        }
+
+        public async Task<IByteBuffer> ResolveImageAsync(HttpContext context, ILogger logger)
+        {
+            //logger.LogInformation($"[AzureBlobImageResolver] Reading {filename} from Azure");
+
+            var filename = context.Request.Path.Value.TrimStart('/');
+
+            var imageBlob = GetBlobContainer().GetBlockBlobReference(filename);
+
+            if (await imageBlob.ExistsAsync() == false)
+                return null;
+
+            IByteBuffer buffer;
+
+            using (var stream = await imageBlob.OpenReadAsync())
+            {
+                buffer = _bufferManager.Allocate((int)stream.Length);
+                await stream.ReadAsync(buffer.Array, 0, (int)stream.Length);
+            }
+
+            return buffer;
+        }
+
+        public Task<bool> IsValidRequestAsync(HttpContext context, ILogger logger)
+        {
+            //return Task.FromResult(FormatHelpers.GetExtension(_options.Configuration, context.Request.Path) != null);
+            return Task.FromResult(CheckFileName(context.Request.Path));
+        }
+
+        public Func<HttpContext, bool> Match { get; set; } = _ => true;
+
+        public IDictionary<string, string> Settings { get; set; } = new Dictionary<string, string>();
+
+        protected CloudBlobContainer GetBlobContainer()
+        {
+            var storageCredentials = new StorageCredentials(
+                _containerSettings.AccountName,
+                _containerSettings.AccessKey);
+            var cloudStorageAccount = new CloudStorageAccount(storageCredentials, true);
+            var cloudBlobClient = cloudStorageAccount.CreateCloudBlobClient();
+            return cloudBlobClient.GetContainerReference(_containerSettings.ContainerName);
+        }
+
+        /// <summary>Gets the file extension for the given image uri</summary>
+        /// <param name="configuration">The library configuration</param>
+        /// <param name="uri">The full request uri</param>
+        /// <returns>The <see cref="T:System.String" /></returns>
+        public static string GetExtension(Configuration configuration, string uri)
+        {
+            string[] strArray = uri.Split('?');
+            StringValues stringValues;
+            if (strArray.Length > 1 && QueryHelpers.ParseQuery(strArray[1]).TryGetValue("format", out stringValues))
+                return (string)stringValues;
+            string str1 = strArray[0];
+            string str2 = (string)null;
+            int num1 = 0;
+            foreach (IImageFormat imageFormat in configuration.ImageFormats)
+            {
+                foreach (string fileExtension in imageFormat.FileExtensions)
+                {
+                    int num2 = str1.LastIndexOf(string.Format(".{0}", (object)fileExtension), StringComparison.OrdinalIgnoreCase);
+                    if (num2 >= num1)
+                    {
+                        num1 = num2;
+                        str2 = fileExtension;
+                    }
+                }
+            }
+            return str2;
+        }
+
+        public static bool CheckFileName(string uri)
+        {
+            string[] strArray = uri.Split('?');
+            if (Guid.Parse(strArray[0].TrimStart('/')) != Guid.Empty)
+                return true;
+
+            return false;
+        }
+
+
+    }
+
+    /// <summary>
+    ///     Configuration settings for the AzureBlobImageResolver
+    /// </summary>
+    public class AzureBlobContainerSettings
+    {
+        public string AccountName { get; set; }
+        public string AccessKey { get; set; }
+        public string ContainerName { get; set; }
+    }
+}
---
+++ ImageProcessingPOC/Startup.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SixLabors.ImageSharp.Web.Caching;
+using SixLabors.ImageSharp.Web.Commands;
+using SixLabors.ImageSharp.Web.DependencyInjection;
+using SixLabors.ImageSharp.Web.Memory;
+using SixLabors.ImageSharp.Web.Middleware;
+using SixLabors.ImageSharp.Web.Processors;
+using SixLabors.ImageSharp.Web.Resolvers;
+
+namespace ImageProcessingPOC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.TryAddSingleton(new AzureBlobContainerSettings
+            {
+                AccountName = "",
+                AccessKey = "",
+                ContainerName = ""
+            });
+
+
+
+            //services.AddImageSharpCore()
+            //    .SetRequestParser<QueryCollectionRequestParser>()
+            //    .SetBufferManager<PooledBufferManager>()
+            //    .SetCache(provider => new PhysicalFileSystemCache(
+            //        provider.GetRequiredService<IHostingEnvironment>(),
+            //        provider.GetRequiredService<IBufferManager>(),
+            //        provider.GetRequiredService<IOptions<ImageSharpMiddlewareOptions>>())
+            //    {
+            //        Settings =
+            //        {
+            //            [PhysicalFileSystemCache.Folder] = PhysicalFileSystemCache.DefaultCacheFolder,
+            //            [PhysicalFileSystemCache.CheckSourceChanged] = "true"
+            //        }
+            //    })
+            //    .SetCacheHash<CacheHash>()
+            //    .SetAsyncKeyLock<AsyncKeyLock>()
+            //    .AddResolver<PhysicalFileSystemResolver>()
+            //    .AddProcessor<ResizeWebProcessor>()
+            //    .AddProcessor<FormatWebProcessor>()
+            //    .AddProcessor<BackgroundColorWebProcessor>();
+
+            //// Add the default service and options.
+            services.AddImageSharpCore()
+                .SetRequestParser<QueryCollectionRequestParser>()
+                .SetBufferManager<PooledBufferManager>()
+                .SetCache<PhysicalFileSystemCache>()
+                .SetCacheHash<CacheHash>()
+                .SetAsyncKeyLock<AsyncKeyLock>()
+                //.AddResolver<PhysicalFileSystemResolver>()
+                .AddResolver<AzureBlobImageResolver>()
+                .AddProcessor<ResizeWebProcessor>();
+
+            //// Or add the default service and custom options.
+            //services.AddImageSharp(
+            //    options =>
+            //        {
+            //            options.Configuration = Configuration.Default;
+            //            options.MaxBrowserCacheDays = 7;
+            //            options.MaxCacheDays = 365;
+            //            options.CachedNameLength = 8;
+            //            options.OnValidate = _ => { };
+            //            options.OnBeforeSave = _ => { };
+            //            options.OnProcessed = _ => { };
+            //            options.OnPrepareResponse = _ => { };
+            //        });
+
+            //// Or we can fine-grain control adding the default options and configure all other services.
+            //services.AddImageSharpCore()
+            //        .SetRequestParser<QueryCollectionRequestParser>()
+            //        .SetBufferManager<PooledBufferManager>()
+            //        .SetCache<PhysicalFileSystemCache>()
+            //        .SetCacheHash<CacheHash>()
+            //        .SetAsyncKeyLock<AsyncKeyLock>()
+            //        .AddResolver<PhysicalFileSystemResolver>()
+            //        .AddProcessor<ResizeWebProcessor>()
+            //        .AddProcessor<FormatWebProcessor>()
+            //        .AddProcessor<BackgroundColorWebProcessor>();
+
+
+            //// Or we can fine-grain control adding custom options and configure all other services
+            //// There are also factory methods for each builder that will allow building from configuration files.
+            //services.AddImageSharpCore(
+            //    options =>
+            //        {
+            //            options.Configuration = Configuration.Default;
+            //            options.MaxBrowserCacheDays = 7;
+            //            options.MaxCacheDays = 365;
+            //            options.CachedNameLength = 8;
+            //            options.OnValidate = _ => { };
+            //            options.OnBeforeSave = _ => { };
+            //            options.OnProcessed = _ => { };
+            //            options.OnPrepareResponse = _ => { };
+            //        })
+            //    .SetRequestParser<QueryCollectionRequestParser>()
+            //    .SetBufferManager<PooledBufferManager>()
+            //    .SetCache(provider =>
+            //      {
+            //          var p = new PhysicalFileSystemCache(
+            //              provider.GetRequiredService<IHostingEnvironment>(),
+            //              provider.GetRequiredService<IBufferManager>(),
+            //              provider.GetRequiredService<IOptions<ImageSharpMiddlewareOptions>>());
+
+            //          p.Settings[PhysicalFileSystemCache.Folder] = PhysicalFileSystemCache.DefaultCacheFolder;
+            //          p.Settings[PhysicalFileSystemCache.CheckSourceChanged] = "true";
+
+            //          return p;
+            //      })
+            //    .SetCacheHash<CacheHash>()
+            //    .SetAsyncKeyLock<AsyncKeyLock>()
+            //    .AddResolver<PhysicalFileSystemResolver>()
+            //    .AddProcessor<ResizeWebProcessor>()
+            //    .AddProcessor<FormatWebProcessor>()
+            //    .AddProcessor<BackgroundColorWebProcessor>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            //app.UseImageSharp();
+
+            app.Map("/media", HandleMapTest1);
+
+            app.UseMvc();
+            //app.UseDefaultFiles();
+
+            //app.UseStaticFiles();
+        }
+
+        private static void HandleMapTest1(IApplicationBuilder app)
+        {
+            app.UseImageSharp();
+        }
+    }
+}
